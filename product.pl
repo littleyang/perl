87acb6bd58f36f485412038e2cdf47445764a22c
littleyang <littleyangqh@gmail.com>
2013-08-29 14:04:08 +0800

    Create product.pl

---
+++ product.pl
@@ -0,0 +1,6 @@
+print "please input a number: \n";
+$first = <STDIN>;
+print "please input a new number: \n";
+$second = <STDIN>;
+$product = $first*$second;
+print "the product is: ".$product."\n";
